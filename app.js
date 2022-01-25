203a534b40d88aeb68334ced164a19ff4501f22a
Om Dalavi <omdalvi184@gmail.com>
2022-01-25 21:06:30 +0530

    third

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.post("/contactMe",(req,res)=>{
 });
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "public", "index.html"));
 });
 
 app.listen(port,()=>{
